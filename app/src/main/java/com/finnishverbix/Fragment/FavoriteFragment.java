18ef2e3390b0b21bdaad4e4c7ba8c5ea6c4e7431
longtran2006 <longtran206@gmail.com>
2015-04-16 16:54:26 +0300

    Doing Swipe Dismiss Recycle View

--- app/src/main/java/com/finnishverbix/Fragment/FavoriteFragment.java
+++ app/src/main/java/com/finnishverbix/Fragment/FavoriteFragment.java
@@ -9,12 +9,15 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+
 import com.finnishverbix.FavoriteFragment.FavoriteRecycleAdapter;
+import com.finnishverbix.FavoriteFragment.SwipeDismissRecyclerViewTouchListener;
 import com.finnishverbix.FavoriteFragment.WordItem;
 import com.finnishverbix.FavoriteFragment.WordListAdapter;
 import com.finnishverbix.R;
@@ -22,6 +25,8 @@ import com.finnishverbix.SQL.SqliteHandler;
 import com.finnishverbix.WordReviewActivity;
 
 import java.util.ArrayList;
+
+import me.drakeet.materialdialog.MaterialDialog;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -39,6 +44,7 @@ public class FavoriteFragment extends Fragment  {
 
     FavoriteRecycleAdapter favoriteRecycleAdapter;
     RecyclerView recyclerView;
+    private int swipe_position;
 
     public FavoriteFragment() {
         // Required empty public constructor
@@ -82,38 +88,80 @@ public class FavoriteFragment extends Fragment  {
         }
         @Override
         protected void onPostExecute(Void args) {
-            favoriteRecycleAdapter = new FavoriteRecycleAdapter(getActivity(),wordList);
-
-            recyclerView.setAdapter(favoriteRecycleAdapter);
             recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-            favoriteRecycleAdapter.setClickListener(new FavoriteRecycleAdapter.ClickListenerCardView() {
+            favoriteRecycleAdapter = new FavoriteRecycleAdapter(getActivity(),wordList);
+            recyclerView.setAdapter(favoriteRecycleAdapter);
+
+            /*favoriteRecycleAdapter.setClickListener(new FavoriteRecycleAdapter.ClickListenerCardView() {
                 @Override
                 public void itemClicked(View view, int position) {
-                    WordItem wordItem = wordList.get(position);
-                    Intent intent = new Intent(rootView.getContext(), WordReviewActivity.class);
-                    intent.putExtra("Verb", wordItem.getVerb());
-                    intent.putExtra("Meaning", wordItem.getMeaning());
-                    intent.putExtra("Type", wordItem.getType());
-                    intent.putExtra("Present", wordItem.getPresent());
-                    intent.putExtra("Perfect", wordItem.getPerfect());
-                    intent.putExtra("Imperfect", wordItem.getImperfect());
-                    intent.putExtra("Pluperfect", wordItem.getPluperfect());
-                    intent.putExtra("Potential", wordItem.getPotential());
-                    intent.putExtra("PotentialPerfect", wordItem.getPotentialperfect());
-                    intent.putExtra("Conditional", wordItem.getConditional());
-                    intent.putExtra("Infinitive2", wordItem.getInfinitive2());
-                    intent.putExtra("Infinitive3", wordItem.getInfinitive3());
-                    rootView.getContext().startActivity(intent);
+                    singleItemIntent(position);
                 }
-            });
+            });*/
+
+            //Handle Swipe Dismiss
+            SwipeDismissRecyclerViewTouchListener touchListener =
+                    new SwipeDismissRecyclerViewTouchListener(recyclerView,
+                            new SwipeDismissRecyclerViewTouchListener.DismissCallbacks() {
+                                @Override
+                                public boolean canDismiss(int position) {
+                                    return true;
+                                }
+
+                                @Override
+                                public void onDismiss(RecyclerView recyclerView, int[] reverseSortedPositions) {
+                                    for(int position : reverseSortedPositions){
+                                        swipe_position = position;
+                                        Log.d("TOUCH ","TOUCH");
+                                        ConfirmationDiaglogBeforeDelete();
+                                    }
+                                }
+                            });
             //wordListAdapter = new WordListAdapter(getActivity(),wordList);
           //  listView.setAdapter(wordListAdapter);
             // Close the progressdialog
-
+            recyclerView.setOnTouchListener(touchListener);
             mProgressDialog.dismiss();
         }
     }
 
+    private void ConfirmationDiaglogBeforeDelete() {
+        WordItem item = wordList.get(swipe_position);
+        final MaterialDialog delete_confirm_dialog;
+        delete_confirm_dialog = new MaterialDialog(getActivity());
+        delete_confirm_dialog.setTitle("DELETE")
+        .setMessage("Are you sure to delete this card:" + item.getVerb() + " ?")
+        .setPositiveButton("YES", new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                delete_confirm_dialog.dismiss();
+            }
+        }).setNegativeButton("NO", new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                delete_confirm_dialog.dismiss();
+            }
+        });
+        delete_confirm_dialog.show();
+    }
+
+    private void singleItemIntent(int position) {
+        WordItem wordItem = wordList.get(position);
+        Intent intent = new Intent(rootView.getContext(), WordReviewActivity.class);
+        intent.putExtra("Verb", wordItem.getVerb());
+        intent.putExtra("Meaning", wordItem.getMeaning());
+        intent.putExtra("Type", wordItem.getType());
+        intent.putExtra("Present", wordItem.getPresent());
+        intent.putExtra("Perfect", wordItem.getPerfect());
+        intent.putExtra("Imperfect", wordItem.getImperfect());
+        intent.putExtra("Pluperfect", wordItem.getPluperfect());
+        intent.putExtra("Potential", wordItem.getPotential());
+        intent.putExtra("PotentialPerfect", wordItem.getPotentialperfect());
+        intent.putExtra("Conditional", wordItem.getConditional());
+        intent.putExtra("Infinitive2", wordItem.getInfinitive2());
+        intent.putExtra("Infinitive3", wordItem.getInfinitive3());
+        rootView.getContext().startActivity(intent);
+    }
     private void showList() {
         wordList = new ArrayList<WordItem>();
         wordList.clear();;
